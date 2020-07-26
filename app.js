2407966e23a9420bba9cfb6c6d756335318bef9f
peacefrog <gregroyclark@gmail.com>
2020-07-25 21:15:09 -0500

    starts basic express server, imports routes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var express = require("express");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./api-routes");
+
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
