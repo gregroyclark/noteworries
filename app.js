a21fff3c9fcc90fece1025634d3e7043e1ac4b14
peacefrog <gregroyclark@gmail.com>
2020-07-25 21:36:33 -0500

    uptades routes import path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./api-routes")(app);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, function() {
     console.log("App is listening on PORT: " + PORT);
